c5025b351f6d82b6ad954bc57fabc609741500bb
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-07-10 22:22:39 +1200

    Update API to connect to database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,31 @@
+// Express
 const express = require("express");
+// PG (Postgre SQL library)
+const { Pool, Client } = require("pg");
+require("dotenv").config();
+
+// Connection String read with dotenc
+const connectionString = process.env.CONNECTIONSTRING;
+
+const pool = new Pool({
+  connectionString,
+});
+
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
+
+// App constructor
 const app = express();
 const port = 3000;
 
+// First basic endpoint
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Listening on: localhost:3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
