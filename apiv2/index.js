242900ea1c77e96dbfb16ea7f62a526cb179ad38
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-07-19 03:20:39 +1200

    Made a lot of changed to apiV2

---
+++ apiv2/index.js
@@ -0,0 +1,45 @@
+// This is like imports in python:
+// We are saving the package as Express and Cors into their own variables. 'express' and 'cors'
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config(); // This is the dotenv package, so we can read an environment file and variables then save them to a var
+
+// Below are all the routers we will be using, basically these are routes that we made but are in different files.
+const getAllUsers = require("./routes/get/getAllUsers");
+
+// Here is a constructor for express so we don't have to use express() whenever making a new
+// endpoint e.g
+/// express().get...
+/// where as we cal just use > app.get("/", (req, res) => ) which is also a fancy callback function
+const app = express();
+app.use(express.json()); // This is using express.json so we can accept json as a valid body
+
+// Route Uses
+app.use("/get", getAllUsers);
+
+// Environment Variables
+const name = process.env.NAME;
+const password = process.env.PASSWORD;
+
+// This is the 'https://localhost:3000/' root endpoint to make sure the user is valid, and he know's
+// what to do next
+app.get("/", async (req, res) => {
+  // In this endpoint we are accept 2 body json keys: Name, and Password, this will be the new
+  // authentication system
+  if (req.body.name === name) {
+    if (req.body.password == password) {
+      res.json({
+        result: "Please choose a real endpoint. Check the docs for more info",
+      });
+    } else {
+      res.json({ result: "Wrong password." });
+    }
+  } else {
+    res.json({ result: "Wrong username." });
+  }
+});
+
+// This is to make the app listen on port 3000 of localhost.
+app.listen(3000, () => {
+  console.log("API listening on port " + 3000);
+});
---
+++ apiv2/routes/get/getAllUsers.js
@@ -0,0 +1,55 @@
+// Like in the index.js page we are importing the express library as a var called express
+const express = require("express");
+let router = express.Router(); // This is a route, we are simply just using a let var and exporting it so we can split up our code and makeing it easier to read
+// This is the node js lirary I've chosen to go with to query my SQL server
+const { Pool, Client } = require("pg");
+require("dotenv").config();
+
+// Here we can accept json as a valid body object
+router.use(express.json());
+
+// These are the varaibles from the .env File and saving them as constants since they will never change
+const name = process.env.NAME;
+const password = process.env.PASSWORD;
+// This is the connection string to my Database, I am using this with the PG node js library so
+// I can query the database. Also, the database is a MySQL Postgre server
+const connectionString = process.env.CONNECTIONSTRING;
+
+// Here is the endpoint to get all the users from the database
+// it takes 2 parameters name, and password
+// @req.body param name;
+// @req.body.param password;
+// req is the name I've given to the request variable.
+// res is the name I've given to the response varaible.
+router.get("/", (req, res) => {
+  // Checking if we have the right name and password (check .env file for it)
+  if (req.body.name === name) {
+    if (req.body.password === password) {
+      // Here I am creating a constructor with the value of @connectionString const so I can connect
+      const client = new Client({
+        connectionString,
+      });
+
+      client.connect();
+      // This is the query callback function, it takes 1 parameter
+      // @query 'SELECT ...' or 'INSERT INTO...' it can also take parameters with the $1 variable
+      // client.query (query, values)...
+      // A call back is a new form of ES6 Javascript syntax
+      // Old syntax:
+      // client.query('SELECT...' function(req, sqlRes) {...})
+      // New syntax
+      // client.query('SELECT...',  (req, sqlRes) => {})
+      client.query('SELECT * FROM "public"."users"', (err, sqlRes) => {
+        res.json(sqlRes.rows); // This is getting all the rows returned from the table 'users'
+        client.end();
+      });
+    } else {
+      res.json({ result: "Wrong password." });
+    }
+  } else {
+    res.json({ result: "Wrong username." });
+  }
+});
+
+// Exporting the module so we can use this from the index.html page
+module.exports = router;
