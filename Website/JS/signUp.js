1d1e5ed8465d9e7f0d56e2581264e4265c887f1f
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2022-07-25 10:44:58 +1200

    Update

--- Website/JS/signUp.js
+++ Website/JS/signUp.js
@@ -1,16 +1,29 @@
 function getDataPostUser() {
+  let tokenGlobal = "";
+
   const firstName = document.getElementById("FirstName").value;
   const lastName = document.getElementById("LastName").value;
   const userName = document.getElementById("Username").value;
   const email = document.getElementById("Email").value;
   const password = document.getElementById("Password").value;
 
-  postUser();
+  getToken()
+    .then((token) => {
+      tokenGlobal = token;
+    })
+    .catch((error) => {
+      console.log("error!");
+      console.error(error);
+    });
+
+  postUser(tokenGlobal);
 }
 
-function postUser() {
+function postUser(token) {
+  console.log(`Bearer ${toString(token)}`);
+
   var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer {token}");
+  myHeaders.append("Authorization", `Bearer ${token}`);
 
   var requestOptions = {
     method: "GET",
